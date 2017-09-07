696402fbb38746762c61af3970f794445da130a5
samyakvardhman <jain.00samyak@gmail.com>
2017-09-07 09:58:30 +0530

    Add files via upload

---
+++ A-5.twinprime.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int prime(int n)
+{
+	int i,f=1;
+	for(i=2;i<n;i++)
+	{
+		if(n%i==0)
+		{
+			f=0;
+			break;	
+		}
+	}
+	if(f&&n!=1)
+	return 1;
+	else
+	return 0;
+}
+int main()
+{
+	int f,s;
+	cout<<"Enter the two prime numbers";
+	cin>>f>>s;
+	if(s-f==2)
+	{
+		if(prime(s)&&prime(f))
+		cout<<"pair is Twin Prime"<<endl;
+		else
+		cout<<"Pair is not Twin Prime"<<endl;
+	}
+	else
+	cout<<"Pair is not Twin Prime"<<endl;
+}
